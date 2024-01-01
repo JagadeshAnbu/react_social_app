f58014c884734be5bdeeac2695614014b4dca1f0
JagadeshAnbu <tajagadesh@gmail.com>
2024-01-01 23:45:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About";
+import Home from "./Home";
+import Missing from "./Missing";
+import NewPost from "./NewPost";
+import Nav from "./Nav";
+import EditPost from "./EditPost";
+import PostPage from "./PostPage";
+import {DataProvider} from "./context/DataContext";
 
-function App() {
+function App(){
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+        <Header title="Vee Social Media"/> 
+        <Nav/>
+
+        <Routes>
+          <Route path="/" element = {
+          <Home/>}/>
+          <Route path="post">
+            <Route index element= {<NewPost/>}/>
+            <Route path=":id" element={<PostPage/>}/>
+          </Route> 
+          <Route path="/edit/:id" element ={<EditPost/>}/>
+          <Route path="about" element={ <About/>}/>
+          <Route path ="*" element ={<Missing/>}/>
+        </Routes>
+        <Footer/>
+      </DataProvider>
     </div>
   );
 }
